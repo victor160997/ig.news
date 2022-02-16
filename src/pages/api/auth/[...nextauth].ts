46206bde7e3da6cb476a15165315d50920bfb388
victor160997 <victor.men.dev@gmail.com>
2022-02-16 19:29:05 -0300

    integraçao com faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from "faunadb"
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -8,4 +11,39 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { userMail: user.email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+      }
+
+      catch {
+        return false
+      }
+    },
+  }
 })
